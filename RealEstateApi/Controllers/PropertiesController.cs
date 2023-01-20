8b4009e5013205235933dcefe8f7e2b21c060958
sshru <sshru@DESKTOP-86NI6JC>
2023-01-20 18:30:14 +0530

    Add project files.

---
+++ RealEstateApi/Controllers/PropertiesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstateApi.Data;
+using RealEstateApi.Models;
+using System.Security.Claims;
+
+namespace RealEstateApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertiesController : ControllerBase
+    {
+        ApiDbContext _dbContext = new ApiDbContext();
+        [HttpPost]
+        [Authorize]
+        public IActionResult Post([FromBody] Property property)
+        {
+            if (property == null)
+            {
+                return NoContent();
+            }
+            else
+            {
+                var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+                var user = _dbContext.Users.First(u => u.Email == userEmail);
+                if (user == null)
+                    return NotFound();
+
+                property.IsTrending = false;
+                property.UserId= user.Id;
+                _dbContext.Properties.Add(property);
+                _dbContext.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created);
+            }
+        }
+    }
+}
